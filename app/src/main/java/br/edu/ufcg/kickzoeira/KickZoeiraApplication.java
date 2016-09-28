8f31200aaf43bdb7556cf5ceb23d42838c650d68
Jordao Serafim <jordaoesa@gmail.com>
2016-09-28 00:24:47 -0300

    criacao e login de usuario OK

---
+++ app/src/main/java/br/edu/ufcg/kickzoeira/KickZoeiraApplication.java
@@ -0,0 +1,15 @@
+package br.edu.ufcg.kickzoeira;
+
+import android.app.Application;
+
+
+/**
+ * Created by jordaoesa on 9/27/16.
+ */
+public class KickZoeiraApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/br/edu/ufcg/kickzoeira/activities/LoginActivity.java
@@ -0,0 +1,105 @@
+package br.edu.ufcg.kickzoeira.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import br.edu.ufcg.kickzoeira.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private final String TAG = "LOGIN";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText edtEmail;
+    private EditText edtPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtEmail = (EditText) findViewById(R.id.editText);
+        edtPass = (EditText) findViewById(R.id.editText2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void handleLogin(View view){
+
+
+        String email = edtEmail.getText().toString();
+        String password = edtPass.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, "falha",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
+                            Intent it = new Intent(getApplicationContext(), ProfileActivity.class);
+                            startActivity(it);
+                        }
+
+                    }
+                });
+
+
+    }
+
+
+    public void handleSingUp(View view){
+        Intent it = new Intent(getApplicationContext(), NewUserActivity.class);
+        startActivity(it);
+    }
+}
